1213662ae78f9b267a8df320e85b7975b80d07c9
Azowyl <facugerstner_29@hotmail.com>
2020-05-05 16:13:09 -0300

    test for file existence

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -3,7 +3,13 @@ require_relative '../src/parser'
 describe Parser do
     context "when file does not exist" do
         it "raises error" do
-            expect{Parser.new('non_existin.json')}.to raise_error ArgumentError
+            expect{Parser.new('non_existin.json')}.to raise_error Errno::ENOENT
+        end
+    end
+
+    context "when file exists" do
+        it "doesn't raise error" do
+            expect{Parser.new('fixtures/test.json')}.to_not raise_error ArgumentError
         end
     end
 end
--- src/parser.rb
+++ src/parser.rb
@@ -1,2 +1,5 @@
 class Parser
+    def initialize(file_name)
+        File.open(file_name)
+    end
 end
