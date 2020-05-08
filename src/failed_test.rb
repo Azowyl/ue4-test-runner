4e221f0a22df718543fa80f67e59e9cc12efe440
azowyl <facugerstner_29@hotmail.com>
2020-05-08 11:19:13 -0300

    add color!

--- src/failed_test.rb
+++ src/failed_test.rb
@@ -1,12 +1,16 @@
 require_relative 'base_test'
+require 'rainbow/refinement'
+using Rainbow
 
 class FailedTest < BaseTest
+  BASE_MESSAGE = "Fail".bg(:red)
+
   def success?
     false
   end
 
   def pretty
-    message = "FAIL -> #{name} \n"
+    message = "#{BASE_MESSAGE} -> #{name} \n"
     entries.each {|entry| message << entry.pretty}
     message
   end
